2915b0917584dc919df25797fc178d5695d63890
Vinz Apside <vinzapside@gmail.com>
2018-10-01 13:57:52 +0200

    ajout de fonction async await

---
+++ app-promises.js
@@ -0,0 +1,105 @@
+const users = [
+    {
+        id: 1,
+        name: 'Andrew',
+        schoolId: 101
+    },
+    {
+        id: 2,
+        name: 'Jessica',
+        schoolId: 999
+    }
+];
+const grades = [{
+    id: 1,
+    schoolId: 101,
+    grade: 86
+}, {
+    id: 2,
+    schoolId: 999,
+    grade: 100
+}, {
+    id: 3,
+    schoolId: 101,
+    grade: 80
+}
+
+];
+
+const getUser = (id) => {
+    return new Promise((resolve, reject) => {
+        const user = users.find((user) => user.id === id);
+        if (user) {
+            resolve(user)
+        }
+        else {
+            reject(`Unable to find user with id of ${id}`);
+        }
+
+    });
+};
+
+const getGrades = (schoolId) => {
+    return new Promise((resolve, reject) => {
+        resolve(grades.filter((grade) => grade.schoolId === schoolId));
+    });
+};
+
+// //Andrew has a 83% in the class
+// const getStatus = (userId) => {
+//     let user;
+//     return getUser(userId).then((tempuser) => {
+//         user = tempuser;
+//         return getGrades(user.schoolId)
+//     }).then((grades) => {
+//         let average = 0;
+//         if (grades.length > 0) {
+//             average = grades.map((grade) => grade.grade).reduce((a, b) => {
+//                 return (a + b) / grades.length;
+//             })
+//             return `${user.name} has a ${average} in the class`;
+//             console.log(average);
+//         }
+//         //average
+//         //return our string
+//     })
+// }
+
+
+//async await....
+const getStatusAlt = async (userId) => {
+    const user = await getUser(userId);
+    const grades = await getGrades(user.schoolId);
+    // console.log(user, grades);
+    let average = 0;
+    if (grades.length > 0) {
+        average = grades.map((grade) => grade.grade).reduce((a, b) => a + b) / grades.length;
+
+        return `${user.name} has a ${average} in the class`;
+    };
+}
+// console.log(getStatusAlt());
+
+// getUser(2).then((user) => {
+//     console.log(user);
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+// getGrades(999).then((grades) => {
+//     console.log(grades);
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+// getStatus(1).then((status) => {
+//     console.log(status);
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+getStatusAlt(1).then((status) => {
+    console.log(status);
+}).catch((e) => {
+    console.log(e);
+})
