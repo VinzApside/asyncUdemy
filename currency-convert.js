e60749ba765f4620e9e2856c23926262abc846c5
Vinz Apside <vinzapside@gmail.com>
2018-11-15 17:15:31 +0100

    async await function

--- currency-convert.js
+++ currency-convert.js
@@ -6,13 +6,15 @@
 const axios = require('axios');
 const url = 'http://data.fixer.io/api/latest?access_key=abf05bf6065bd7fd6eaec2406fb9df2f';
 
-const getExchangeRate = (from, to) => {
-    return axios.get(url).then((response) => {
-        const euro = 1 / response.data.rates[from];
-        const rate = euro * response.data.rates[to];
-        return rate;
-    })
+const getExchangeRate = async (from, to) => {
+    const response = await axios.get(url);
+    const euro = 1 / response.data.rates[from];
+    const rate = euro * response.data.rates[to];
+    return rate;
+
 };
+
+
 
 getExchangeRate('USD', 'CAD').then((rate) => {
     console.log(rate);
